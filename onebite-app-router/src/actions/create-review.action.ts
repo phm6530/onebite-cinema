8bb2fc97320bb899a2a5d3e207781fbb0e79b989
phm6530 <squirrel309@naver.com>
2024-10-03 11:44:26 +0900

    [day 17] useActionState 반영

--- onebite-app-router/src/actions/create-review.action.ts
+++ onebite-app-router/src/actions/create-review.action.ts
@@ -1,18 +1,24 @@
+"use server";
 import { BASE_URL } from "@/config/baseUrl";
 import { withFetch } from "@/lib/fetch";
+import delay from "@/util/delay";
+import { error } from "console";
 import { revalidatePath } from "next/cache";
 
-const postReviewAction = async (formData: FormData) => {
-  "use server";
+const postReviewAction = async (state: any, formData: FormData) => {
   const movieId = formData.get("movieId")?.toString();
   const author = formData.get("author")?.toString();
   const content = formData.get("content")?.toString();
 
-  if (!author || !content) {
-    return;
+  if (!author || !content || !movieId) {
+    return {
+      status: false,
+      error: "리뷰 내용을 설정해주세요",
+    };
   }
 
   await withFetch(async () => {
+    await delay(2000);
     return fetch(`${BASE_URL}/review`, {
       method: "POST",
       body: JSON.stringify({
